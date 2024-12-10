cd2d5e44134a3e84d84cbdff0738af993ced93bc
edniea <ajednie11@gmail.com>
2024-12-10 13:54:52 -0500

    Fixing bad VenueRoutes

--- server/routes/venueRoutes.js
+++ server/routes/venueRoutes.js
@@ -7,8 +7,7 @@ import {
   getVenueProfile,
   updateVenueProfile,
   createEvent,
-  editEvent,
-  getEvents
+  editEvent
 } from '../controllers/venueController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -23,15 +22,5 @@ router
   .get(protect, getVenueProfile)
   .put(protect, updateVenueProfile);
 
-router
-  .route('/events')
-  .get(protect, getEvents)    // This route gets the events for the venue (optional, based on your needs)
-  .post(protect, createEvent);
-
-router
-  .route('/events/:eventId')
-  .put(protect, editEvent)    // Edit an event by eventId
-  .get(protect, getEvent)     // If you need to view a single event (optional)
-  .delete(protect, deleteEvent); // If you plan to allow event deletion (optional)
 
 export default router;
