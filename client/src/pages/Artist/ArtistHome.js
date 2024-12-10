803f776de449efbfa09e88bb8d40b3c3dc22e977
edniea <ajednie11@gmail.com>
2024-12-10 15:32:21 -0500

    Let artists create events

--- client/src/pages/Artist/ArtistHome.js
+++ client/src/pages/Artist/ArtistHome.js
@@ -5,9 +5,15 @@ const ArtistHome = () => {
   return (
     <div>
       <h1>Artist Home</h1>
-      {/* Use Link to navigate to the artist list page */}
+
+      {/* View Artist List Button */}
       <Link to="/artist-list">
         <button>View Artist List</button>
+      </Link>
+
+      {/* Create Event Button */}
+      <Link to="/venue/create-event">
+        <button>Create New Event</button>
       </Link>
     </div>
   );
